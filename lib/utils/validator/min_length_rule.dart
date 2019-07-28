52955c6134ae24262db5e609d8c16e7a32cf6d28
Ibrahim Mubarak <ibrahim@faimtech.in>
2019-07-29 02:48:38 +0530

    Added Email Rule, Min Length Rule and Required Rule, Deleted old Validator, Added test for email rule

---
+++ lib/utils/validator/min_length_rule.dart
@@ -0,0 +1,15 @@
+import 'package:flrx/utils/validator/rule.dart';
+
+class MinLengthRule extends Rule {
+  final int minLength;
+
+  MinLengthRule(this.minLength);
+
+  @override
+  String validate(String entityName, String value) {
+    if (value.length < minLength) {
+      return "$entityName should be more than $minLength characters";
+    }
+    return null;
+  }
+}
---
+++ test/unit/email_rule_test.dart
@@ -0,0 +1,54 @@
+import 'package:flrx/utils/validator/email_rule.dart';
+import 'package:test_api/test_api.dart';
+
+// Test Cases are from Microsoft,
+// https://blogs.msdn.microsoft.com/testing123/2009/02/06/email-address-test-cases/
+void main() {
+  EmailRule rule = EmailRule();
+  List<String> validEmailIds = <String>[
+    'abcd@example.com',
+    'firstname.lastname@domain.com',
+    'email@subdomain.domain.com',
+    'firstname+lastname@domain.com',
+    'email@123.123.123.123',
+    '1234567890@domain.com',
+    'email@domain-one.com',
+    '_______@domain.com',
+    'email@domain.name',
+    'email@domain.co.jp',
+    'firstname-lastname@domain.com',
+    //  TODO These 2 cases should pass
+    // 'email@[123.123.123.123]',
+    // '"email"@domain.com',
+  ];
+  List<String> invalidEmailIds = <String>[
+    'plainaddress',
+    'abcd@example,com',
+    r'#@%^%#$@#$@#.com',
+    '@domain.com',
+    'Joe Smith <email@domain.com>',
+    'email.domain.com',
+    'email@domain@domain.com',
+    'あいうえお@domain.com',
+    'email@domain.com (Joe Smith)',
+    'email@-domain.com',
+    'email@domain..com',
+    //  TODO The following test cases should pass
+    //  '.email@domain.com',
+    //  'email.@domain.com',
+    //  'email..email@domain.com',
+    //  'email@domain',
+  ];
+  test("valid_email_test", () {
+    validEmailIds.forEach((String emailId) {
+      String validationError = rule.validate("email", emailId);
+      expect(validationError, null);
+    });
+  });
+  test("invalid_email_test", () {
+    invalidEmailIds.forEach((String emailId) {
+      String validationError = rule.validate("email", emailId);
+      expect(validationError, "$emailId is not a valid email address");
+    });
+  });
+}
