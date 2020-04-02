c0b534fb0846373556c9f50053a8c7cb9acf8614
Ibrahim Mubarak <ibrahim@faimtech.in>
2020-04-02 22:49:03 +0530

    Remove ApiClient, Dio, Redux Future

--- lib/flrx.dart
+++ lib/flrx.dart
@@ -7,7 +7,6 @@ library flrx;
 
 export 'pages/page.dart';
 export 'pages/viewmodel.dart';
-export 'api/api_client.dart';
 export 'application.dart';
 export 'components/error/error.dart';
 export 'components/localization/localization.dart';
@@ -15,6 +14,5 @@ export 'components/logger/logger.dart';
 export 'config/config.dart';
 export 'navigation/route_retriever.dart';
 export 'navigation/router.dart';
-export 'store/middlewares/future.dart';
 export 'store/store_retriever.dart';
 export 'widgets/flavor_app.dart';
