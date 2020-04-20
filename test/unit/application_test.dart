ad91080546d5ad1fa6f53c95ae8d5c75599d2d2c
Ibrahim Mubarak <ibrahim@faimtech.in>
2020-04-20 20:31:26 +0530

    Add test to see if Application is initializing Module

---
+++ test/unit/application_test.dart
@@ -0,0 +1,18 @@
+import 'package:flrx/application.dart';
+import 'package:flrx/components/error/error.dart';
+import 'package:test/test.dart';
+
+import '../mocks/mock_error_reporter.dart';
+import '../mocks/mock_module.dart';
+
+void main() {
+  test("That Application can initalize Modules", () {
+
+    Application.init(() {}, modules: [
+      MockModule()
+    ]);
+
+    ErrorReporter reporter = Application.get<ErrorReporter>();
+    expect(reporter is MockErrorReporter, true);
+  });
+}
--- test/unit/module_test.dart
+++ test/unit/module_test.dart
@@ -1,38 +1,13 @@
 import 'package:flrx/application.dart';
 import 'package:flrx/components/modules/module.dart';
 import 'package:flrx/flrx.dart';
-import 'package:flrx/navigation/widget_route.dart';
+import 'package:fluro/fluro.dart';
 import 'package:fluro/src/router.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:test/test.dart';
 
-class MockModuleWithNamespacedRoute extends MockModule {
-  @override
-  bool get shouldNamespaceRoutes => true;
-}
-
-class MockModule extends Module {
-  @override
-  String get name => "mock";
-
-  @override
-  bool get shouldNamespaceRoutes => false;
-
-  @override
-  Map<String, WidgetRoute> routes() {
-    return {
-      "/": WidgetRoute((args) => Container()),
-      "/mock": WidgetRoute((args) => Scaffold()),
-    };
-  }
-
-  @override
-  Future<void> onInit() async {
-    locator.registerSingleton<Divider>(Divider());
-  }
-}
+import '../mocks/mock_error_reporter.dart';
+import '../mocks/mock_module.dart';
 
 void main() {
   setUp(() {
@@ -45,8 +20,8 @@ void main() {
 
     await module.initialize();
 
-    Divider divider = Application.get<Divider>();
-    expect(divider is Divider, true);
+    ErrorReporter reporter = Application.get<ErrorReporter>();
+    expect(reporter is MockErrorReporter, true);
   });
 
   test("That modules can register routes", () async {
@@ -55,6 +30,7 @@ void main() {
 
     await module.initialize();
 
+    expect(module.shouldNamespaceRoutes, false);
     expect(router.match("/").route.handler != null, true);
     expect(router.match("/mock").route.handler != null, true);
   });
@@ -66,17 +42,31 @@ void main() {
 
     await module.initialize();
 
-    expect(router.match("mock/").route.handler != null, true); // 2
-//    expect(router.match("mock").route.handler != null, true); // 2
-    expect(router.match("/mock/").route.handler != null, true); // 1
-//    expect(router.match("/mock").route.handler != null, true);// 1
+    expect(module.shouldNamespaceRoutes, true);
 
-//    expect(router.match("/mock/mock/").route.handler != null, true);
-    expect(router.match("/mock/mock").route.handler != null, true);
-    expect(router.match("mock/mock").route.handler != null, true);
-//    expect(router.match("mock/mock/").route.handler != null, true);
+    Handler handlerForPath(path) => router.match(path)?.route?.handler;
+
+    expect(handlerForPath("mock/") != null, true); // 2
+//    expect(handlerForPath("mock") != null, true); // 2
+    expect(handlerForPath("/mock/") != null, true); // 1
+//    expect(handlerForPath("/mock") != null, true);// 1
+
+//    expect(handlerForPath("/mock/mock/") != null, true);
+    expect(handlerForPath("/mock/mock") != null, true);
+    expect(handlerForPath("mock/mock") != null, true);
+//    expect(handlerForPath("mock/mock/") != null, true);
 
     // /mock/
     // /mock/mock
+  });
+
+  test("That modules can register route that are namespaced", () async {
+    Module module = MockModuleWithInvalidRoutes();
+    try {
+      await module.initialize();
+      fail("Module did not throw exception");
+    } catch (exception) {
+      expect(exception is ArgumentError, true);
+    }
   });
 }
